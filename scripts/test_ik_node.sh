a12c023e77aaf5d9c74ea8d550f2f8fa1f9d55fe
kz-lemon4ik <kz.lemon4ik@gmail.com>
2025-07-04 12:09:02 +0200

    Correct message types and dependencies

--- scripts/test_ik_node.sh
+++ scripts/test_ik_node.sh
@@ -6,18 +6,18 @@ echo ""
 
 # Test reachable targets
 echo "Testing reachable targets:"
-rostopic pub /target_goal geometry_msgs/Point "x: 1.5, y: 0.5, z: 0.0" -1
+rostopic pub /target_goal geometry_msgs/Point '{x: 1.5, y: 0.5, z: 0.0}' -1
 sleep 2
 
-rostopic pub /target_goal geometry_msgs/Point "x: 0.0, y: 1.8, z: 0.0" -1
+rostopic pub /target_goal geometry_msgs/Point '{x: 0.0, y: 1.8, z: 0.0}' -1
 sleep 2
 
-rostopic pub /target_goal geometry_msgs/Point "x: -1.0, y: 1.0, z: 0.0" -1
+rostopic pub /target_goal geometry_msgs/Point '{x: -1.0, y: 1.0, z: 0.0}' -1
 sleep 2
 
 # Test unreachable target
 echo "Testing unreachable target:"
-rostopic pub /target_goal geometry_msgs/Point "x: 3.0, y: 0.0, z: 0.0" -1
+rostopic pub /target_goal geometry_msgs/Point '{x: 3.0, y: 0.0, z: 0.0}' -1
 sleep 2
 
 echo "Test completed. Check the topics:"
